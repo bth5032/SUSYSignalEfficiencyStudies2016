2260b24163593bb8d5a920a03b1401a89ea224eb
Bobak Hashemi <bth5032@gmail.com>
2016-09-30 15:31:39 -0700

    Added proper stuff to make histograms

--- doAll.C
+++ doAll.C
@@ -7,32 +7,8 @@ void runScanChain(ConfigParser* conf){
   cout<<"Using config:"<<endl;
   conf->print();
 
+  ScanChain(getSUSYChain(conf->get("data_set")), conf->get("Name"), conf);   
 
-  if (conf->get("data") == "true"){
-    if (conf->get("zjets") == "true"){
-      ScanChain(getDataZChain(conf->get("data_set")), conf->get("data_type"), conf);  
-    }
-    if (conf->get("gjets") == "true"){
-      ScanChain(getDataPhotonChain(conf->get("data_set")), conf->get("data_type"), conf);  
-    }
-    if (conf->get("ttbar") == "true"){
-      ScanChain(getDataZChain(conf->get("data_set")), conf->get("data_type"), conf);  
-    }
-  }
-  else if (conf->get("rares") == "true"){
-    ScanChain(getRareChain(conf->get("data_set")), conf->get("data_set"), conf);
-  }
-  else{
-    if (conf->get("zjets") == "true") {
-      ScanChain(getZJetsChain(conf->get("data_set")), "zjets", conf); 
-    }
-    if (conf->get("gjets") == "true") {
-      ScanChain(getGJetsChain(conf->get("data_set")), "gjets", conf);  
-    }
-    if (conf->get("ttbar") == "true"){
-      ScanChain(getTTBarMC(conf->get("data_set")), conf->get("Name"), conf);   
-    }
-  }
 }
 
 void doAll ( TString config_name, TString config_file="configs/run_modes.conf" ) {

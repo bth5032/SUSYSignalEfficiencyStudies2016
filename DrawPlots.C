7eace37ebec473dfb3071ec89460ae4cf1d47429
Bobak Hashemi <bth5032@gmail.com>
2016-09-30 18:42:31 -0700

    renamed histos properly

--- DrawPlots.C
+++ DrawPlots.C
@@ -6,22 +6,28 @@ void DrawPlots(TString sample){
 
 	vector<TFile*> files;
 	if (sample == "edge"){
-		files.push_back(TFile::Open(dir_loc+TString("ct_SUSY_EdgeZ.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYLightEdgeZ.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYHeavyEdgeZ.root"), "READ"));
 	}
 	else if (sample=="A_Btag"){
-		files.push_back(TFile::Open(dir_loc+TString("ct_SUSY_A_btag.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYLightA_btag.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYHeavyA_btag.root"), "READ"));
 	}
 	else if (sample=="A_Bveto"){
-		files.push_back(TFile::Open(dir_loc+TString("ct_SUSY_A_bveto.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYLightA_bveto.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYHeavyA_bveto.root"), "READ"));
 	}
 	else if (sample=="B_Btag"){
-		files.push_back(TFile::Open(dir_loc+TString("ct_SUSY_B_btag.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYLightB_btag.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYHeavyB_btag.root"), "READ"));
 	}
 	else if (sample=="B_Bveto"){
-		files.push_back(TFile::Open(dir_loc+TString("ct_SUSY_B_bveto.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYLightB_bveto.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYHeavyB_bveto.root"), "READ"));
 	}
 	else if (sample=="ATLAS"){
-		files.push_back(TFile::Open(dir_loc+TString("ct_SUSY_ATLAS.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYLightATLAS.root"), "READ"));
+		files.push_back(TFile::Open(dir_loc+TString("SUSYHeavyATLAS.root"), "READ"));
 	}
 	else{
 		cout<<"Unrecognized sample: "<<sample<<endl;
@@ -30,35 +36,26 @@ void DrawPlots(TString sample){
 
 //cout<<__LINE__<<endl;
 
-	TH1D * lowVert = (TH1D*) ((TH1D*) files[0]->Get("SUSY_type1MET_widebin_lowVert"))->Clone("lowVert");
-	TH1D * highVert = (TH1D*) ((TH1D*) files[0]->Get("SUSY_type1MET_widebin_highVert"))->Clone("highVert");
+	TH1D * lowVert_light = (TH1D*) ((TH1D*) files[0]->Get("SUSY_type1MET_widebin_lowVert"))->Clone("lowVert_light");
+	TH1D * highVert_light = (TH1D*) ((TH1D*) files[0]->Get("SUSY_type1MET_widebin_highVert"))->Clone("highVert_light");
+
+	TH1D * lowVert_heavy = (TH1D*) ((TH1D*) files[0]->Get("SUSY_type1MET_widebin_lowVert"))->Clone("lowVert_heavy");
+	TH1D * highVert_heavy = (TH1D*) ((TH1D*) files[0]->Get("SUSY_type1MET_widebin_highVert"))->Clone("highVert_heavy");
 
 //cout<<__LINE__<<endl;
 
-	lowVert->SetLineColor(kRed);
-	highVert->SetLineColor(kBlue);
-
-//cout<<__LINE__<<endl;
-
-	z_dilmass_tight->SetLineColor(kRed);
-	z_dilmass_med->SetLineColor(kRed);
-	z_dilmass_loose->SetLineColor(kRed);
-
-	/*z_dilmass_tight->SetMinimum(0);
-	z_dilmass_med->SetMinimum(0);
-	z_dilmass_loose->SetMinimum(0);*/
-
-	tt_dilmass_tight->SetLineColor(kBlue);
-	tt_dilmass_med->SetLineColor(kBlue);
-	tt_dilmass_loose->SetLineColor(kBlue);
+	lowVert_light->SetLineColor(kRed);
+	highVert_light->SetLineColor(kBlue);
+	lowVert_heavy->SetLineColor(kRed);
+	highVert_heavy->SetLineColor(kBlue);
 
 //cout<<__LINE__<<endl;
 
 //===============================
-//Draw Loose Plots
+//Draw Plots
 //===============================
 
-	TCanvas *c1 = new TCanvas("MET_loose", "", 2000, 2000);
+	TCanvas *c1 = new TCanvas("MET_Light_canvas", "", 2000, 2000);
 	c1->cd();
 
 //cout<<__LINE__<<endl;
@@ -66,9 +63,9 @@ void DrawPlots(TString sample){
 	gPad->SetLogy(1);
 	gStyle->SetOptStat(kFALSE);
 
-	z_met_loose->SetTitle("E^{miss}_{T} for 10 GeV Window");
-	z_met_loose->Draw("HIST");
-	tt_met_loose->Draw("HIST SAME");
+	lowVert_light->SetTitle("E^{miss}_{T} by pileup SUSY signals (mLSP=200, mGluino=1300)");
+	lowVert_light->Draw("HIST");
+	highVert_light->Draw("HIST SAME");
 
 //cout<<__LINE__<<endl;
 	
@@ -78,26 +75,26 @@ void DrawPlots(TString sample){
 	l1->SetLineColor(kWhite);  
 	l1->SetShadowColor(kWhite);
 	l1->SetFillColor(kWhite);
-	l1->AddEntry(z_met_loose, "DY MC", "f");
-	l1->AddEntry(tt_met_loose, "TTBar MC", "f");
+	l1->AddEntry(lowVert_light, "Less than 20 verticies", "f");
+	l1->AddEntry(highVert_light, "20 or more verticies", "f");
 	  
 	l1->Draw("same");
 
 //cout<<__LINE__<<endl;
 
-	c1->SaveAs("plots/"+sample+"_loose_MET.png");
+	c1->SaveAs("plots/"+sample+"_MET_light.png");
 
-	TCanvas *c2 = new TCanvas("dilmass_loose", "", 2000, 2000);
+	TCanvas *c2 = new TCanvas("MET_Heavy_canvas", "", 2000, 2000);
 	c2->cd();
 
 //cout<<__LINE__<<endl;
 
-	gPad->SetLogy(0);
+	gPad->SetLogy(1);
 	gStyle->SetOptStat(kFALSE);
 
-	z_dilmass_loose->SetTitle("Dilepton Mass for 10 GeV Window");
-	z_dilmass_loose->Draw("HIST");
-	tt_dilmass_loose->Draw("HIST SAME");
+	lowVert_heavy->SetTitle("E^{miss}_{T} by pileup SUSY signals (mLSP=1000, mGluino=1400)");
+	lowVert_heavy->Draw("HIST");
+	highVert_heavy->Draw("HIST SAME");
 
 //cout<<__LINE__<<endl;
 	
@@ -107,144 +104,18 @@ void DrawPlots(TString sample){
 	l2->SetLineColor(kWhite);  
 	l2->SetShadowColor(kWhite);
 	l2->SetFillColor(kWhite);
-	l2->AddEntry(z_dilmass_loose, "DY MC", "f");
-	l2->AddEntry(tt_dilmass_loose, "TTBar MC", "f");
+	l2->AddEntry(lowVert_heavy, "Less than 20 verticies", "f");
+	l2->AddEntry(highVert_heavy, "20 or more verticies", "f");
 	  
 	l2->Draw("same");
 
 //cout<<__LINE__<<endl;
 
-	c2->SaveAs("plots/"+sample+"_loose_dilmass.png");
-
-//cout<<__LINE__<<endl;
+	c2->SaveAs("plots/"+sample+"_MET_heavy.png");
 
 
-//===============================
-//Draw Medium Plots
-//===============================
-
-	TCanvas *c3 = new TCanvas("MET_med", "", 2000, 2000);
-	c3->cd();
-
-//cout<<__LINE__<<endl;
-
-	gPad->SetLogy(1);
-	gStyle->SetOptStat(kFALSE);
-
-	z_met_med->SetTitle("E^{miss}_{T} for 5 GeV Window");
-	z_met_med->Draw("HIST");
-	tt_met_med->Draw("HIST SAME");
-
-//cout<<__LINE__<<endl;
-	
-	TLegend *l3;
-	l3 = new TLegend(0.73, 0.73, 0.88, 0.88);
-	  
-	l3->SetLineColor(kWhite);  
-	l3->SetShadowColor(kWhite);
-	l3->SetFillColor(kWhite);
-	l3->AddEntry(z_met_med, "DY MC", "f");
-	l3->AddEntry(tt_met_med, "TTBar MC", "f");
-	  
-	l3->Draw("same");
-
-//cout<<__LINE__<<endl;
-
-	c3->SaveAs("plots/"+sample+"_med_MET.png");
-
-	TCanvas *c4 = new TCanvas("dilmass_med", "", 2000, 2000);
-	c4->cd();
-
-//cout<<__LINE__<<endl;
-
-	gPad->SetLogy(0);
-	gStyle->SetOptStat(kFALSE);
-
-	z_dilmass_med->SetTitle("Dilepton Mass for 5 GeV Window");
-	z_dilmass_med->Draw("HIST");
-	tt_dilmass_med->Draw("HIST SAME");
-
-//cout<<__LINE__<<endl;
-	
-	TLegend *l4;
-	l4 = new TLegend(0.73, 0.73, 0.88, 0.88);
-	  
-	l4->SetLineColor(kWhite);  
-	l4->SetShadowColor(kWhite);
-	l4->SetFillColor(kWhite);
-	l4->AddEntry(z_dilmass_med, "DY MC", "f");
-	l4->AddEntry(tt_dilmass_med, "TTBar MC", "f");
-	  
-	l1->Draw("same");
-
-//cout<<__LINE__<<endl;
-
-	c4->SaveAs("plots/"+sample+"_med_dilmass.png");
-
-//cout<<__LINE__<<endl;
-
-//===============================
-//Draw Tight Plots
-//===============================
-
-	TCanvas *c5 = new TCanvas("MET_tight", "", 2000, 2000);
-	c5->cd();
-
-//cout<<__LINE__<<endl;
-
-	gPad->SetLogy(1);
-	gStyle->SetOptStat(kFALSE);
-
-	z_met_tight->SetTitle("E^{miss}_{T} for 2.5 GeV Window");
-	z_met_tight->Draw("HIST");
-	tt_met_tight->Draw("HIST SAME");
-
-//cout<<__LINE__<<endl;
-	
-	TLegend *l5;
-	l5 = new TLegend(0.73, 0.73, 0.88, 0.88);
-	  
-	l5->SetLineColor(kWhite);  
-	l5->SetShadowColor(kWhite);
-	l5->SetFillColor(kWhite);
-	l5->AddEntry(z_met_tight, "DY MC", "f");
-	l5->AddEntry(tt_met_tight, "TTBar MC", "f");
-	  
-	l5->Draw("same");
-
-//cout<<__LINE__<<endl;
-
-	c5->SaveAs("plots/"+sample+"_tight_MET.png");
-
-	TCanvas *c6 = new TCanvas("dilmass_tight", "", 2000, 2000);
-	c6->cd();
-
-//cout<<__LINE__<<endl;
-
-	gPad->SetLogy(0);
-	gStyle->SetOptStat(kFALSE);
-
-	z_dilmass_tight->SetTitle("Dilepton Mass for 2.5 GeV Window");
-	z_dilmass_tight->Draw("HIST");
-	tt_dilmass_tight->Draw("HIST SAME");
-
-//cout<<__LINE__<<endl;
-	
-	TLegend *l6;
-	l6 = new TLegend(0.73, 0.73, 0.88, 0.88);
-	  
-	l6->SetLineColor(kWhite);  
-	l6->SetShadowColor(kWhite);
-	l6->SetFillColor(kWhite);
-	l6->AddEntry(z_dilmass_tight, "DY MC", "f");
-	l6->AddEntry(tt_dilmass_tight, "TTBar MC", "f");
-	  
-	l6->Draw("same");
-
-//cout<<__LINE__<<endl;
-
-	c6->SaveAs("plots/"+sample+"_tight_dilmass.png");
-
-//cout<<__LINE__<<endl;
-
+cout<<"SUSY Light low vertex count MET in bin 300+"<<lowVert_light->GetBinContent(lowVert_light->FindBin(300))<<endl;
+cout<<"SUSY Light high vertex count MET in bin 300+"<<highVert_light->GetBinContent(highVert_light->FindBin(300))<<endl;
+cout<<"SUSY Heavy low vertex count MET in bin 300+"<<lowVert_heavy->GetBinContent(lowVert_heavy->FindBin(300))<<endl;
+cout<<"SUSY Heavy high vertex count MET in bin 300+"<<highVert_heavy->GetBinContent(highVert_heavy->FindBin(300))<<endl;
 }
